db7a575265769e7f1f1a22a5db411ebf88d34c6d
ChayakarnC <100829597+ChayakarnC@users.noreply.github.com>
2022-08-23 14:18:51 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+//var prevScrollpos = window.pageYOffset;
+///window.onscroll = function() {
+  //var currentScrollPos = window.pageYOffset;
+//if (prevScrollpos > currentScrollPos) {
+  //  document.getElementById("header").style.top = "0";
+  //  document.getElementById("nav-logo").style.top = "0";
+   // document.getElementById("nav-bar").style.top = "0";
+//  } 
+ // else {
+//    document.getElementById("header").style.top = "-90%";
+ //   document.getElementById("nav-logo").style.top = "-90%";
+//    document.getElementById("nav-bar").style.top = "-90%";
+//  }
+//  prevScrollpos = currentScrollPos;
+//}
